547cca1bfc1ce6bbeab6338a3a5d1fb8f83ec160
lucaszm <lucas.morais446@hotmail.com>
2019-04-07 00:00:44 +0000

    Funcionando no Terminal - Toas funcoes basicas funcionando

--- main.c
+++ main.c
@@ -7,98 +7,118 @@ typedef struct contato {
     int numero;
 }contato;
 
-
 void menu(void);
 void addContato (void);
-void mostraContatos(void);
+void listaContato(void);
 void rmContato(void);
 void ordena(void);
 
-contato *pessoa;
-void *buffer;
-int *ntotal,*escolha;
-int *acontador,*bcontador;
+void *pBuffer;
+int *ntotal, *escolha, *cont;
+contato *pessoa,*aux;
 
-int main(){
-   buffer = (void*) malloc (4*sizeof(int));
-   acontador = buffer;
-   bcontador = acontador+1;
-   ntotal = bcontador+1;
-   escolha = ntotal +1;
-   pessoa = escolha+1;
-   *ntotal = 0;
-    while(1){
-        menu();
+int main () {
+    pBuffer = malloc(3*sizeof(int));
+    ntotal = pBuffer;
+    escolha = ntotal+1;
+    cont = escolha+1;
+    pessoa = cont+1;
+    *ntotal = 0;
+    do {
+        menu ();
         scanf("%d",escolha);
-        getchar();
-        if     (*escolha==1) addContato();
-        else if(*escolha==2) rmContato();
-        else if(*escolha==3) mostraContatos();
-        else return 0;
-   }
+        getchar;
+        switch (*escolha) {
+        case 1:
+            (*ntotal)++;
+            addContato ();
+            break;
+        case 2:
+            rmContato ();
+            break;
+        case 3:
+            listaContato ();
+            break;
+        default:
+            printf("\n----- Saindo do programa! -----\n\n");
+            return 0;
+        }
+    } while (*escolha != 4);
 }
 
-/*void ordena () {
-    acontador = buffer;
-    bcontador = acontador+1;
-    ntotal = bcontador+1;
-    escolha = ntotal +1;
-    pessoa = escolha + 1;
-    //if(*ntotal != 1) {
-    for ((*acontador) = 0;(*acontador) < (*ntotal); *acontador = (*acontador) + 1) pessoa++;
-    //}
+void addContato () {
+    pBuffer = realloc(pBuffer,(3*sizeof(int)) + ((*ntotal)*sizeof(contato)));
+    ntotal = (int*)pBuffer;
+    escolha = (int*)ntotal+ 1;
+    cont = (int*)escolha+ 1;
+    pessoa = (contato*) (cont + 1);
+    for (*cont = 1; *cont < *ntotal; (*cont) = (*cont) + 1){
+            pessoa++;
+    }
+    printf("\nNome: ");
+    scanf("%s",pessoa->nome);
+    printf("Telefone: ");
+    scanf("%d",&pessoa->numero);
+}
+
+void listaContato () {
+    if (*ntotal == 0) {
+        printf("\nAinda nao ha contatos adicionados\n");
+    }
+    else {
+    pessoa = (int*) cont + 1;
+    for(*cont = 0; *cont < *ntotal; (*cont) = (*cont)+1){
+        printf("\nNome: %s",pessoa->nome);
+	    printf("\nTelefone: %d\n",pessoa->numero);
+	    pessoa = (contato*)pessoa + 1;
+    }
+    }
+}
+
+void rmContato () {
+    if (*ntotal == 0) {
+        printf("\nAinda nao ha contatos adicionados\n");
+    }
+    else {
+    printf("\nDigite o numero do contato que desejas remover: ");
+    scanf("%d",escolha);
+    aux = pessoa - 1;
+    pessoa = (contato*) (cont + 1);
+    for ((*cont)=0;(*cont)<=(*ntotal);(*cont)++) {
+        if ((*escolha) == pessoa->numero) {
+            (*ntotal)--;
+            strcpy(pessoa->nome,aux->nome);
+            pessoa->numero = aux->numero;
+            pBuffer  = (void*) realloc (pBuffer,3*sizeof(int)+(*ntotal)*sizeof(contato));
+            ntotal = (int*)pBuffer;
+            escolha = (int*)ntotal+ 1;
+            cont = (int*)escolha+ 1;
+            pessoa = (contato*) (cont + 1);
+            for (*cont = 1; *cont < *ntotal; (*cont) = (*cont) + 1){
+                    pessoa++;
+            }
+
+        }
+        pessoa++;
+    }
+    }
+}
+
+/*void rmContato () {
+    int *cont2;
+    printf("\nDigite o numero do contato que desejas remover: ");
+    scanf("%d",escolha);
+    for (*cont = 0; *cont < *ntotal; (*cont) = (*cont)+1){
+        if (pessoa->numero == escolha) {
+                for (*cont2 = cont; *cont2 < *ntotal; (*cont) = (*cont) + 1) {
+                    pessoa->numero = (++pessoa)->numero;
+                    strcpy (pessoa->nome;(++pessoa)->nome);
+                    pessoa = (contato*)pessoa + 1;
+                }
+        }
+    }
 }*/
 
 void menu (void) {
     printf("\n\n----- MENU ----\n1 - Adicionar Contato\n2 - Remover Contato\n3 - Listar Contatos\n ");
-}
-
-
-void addContato(void){
-    *ntotal=*ntotal+1;
-    buffer  = (void*) realloc (buffer,4*sizeof(int)+(*ntotal)*sizeof(contato));
-    printf("\nNome: ");
-    scanf("%[^\n]",pessoa->nome);
-    printf("Telefone: ");
-    scanf("%d",&pessoa->numero);
-    pessoa++;
-    //ordena();
-    //if (*ntotal == 1) {
-    //buffer  = (void*) realloc (buffer,4*sizeof(int));
-    //ordena();
-    //addContato();
-    //}
-}
-void rmContato (void) {
-    char remover[20];
-    contato *aux;
-    printf("\nQual Contato desejas removar: ");
-    scanf("%[^\n]",remover);
-    aux = pessoa - 1;
-    pessoa = escolha + 1;
-    for ((*acontador)=0;(*acontador)<=(*ntotal);(*acontador)++) {
-        printf("\nNome pessoa - %s\nNome Aux - %s\n",pessoa->nome,aux->nome);
-        if (strcmp(pessoa->nome,remover) == 0) {
-            printf("\nNome pessoa - %s\nNome Aux - %s\n",pessoa->nome,aux->nome);
-            strcpy(pessoa->nome,aux->nome);
-            pessoa->numero = aux->numero;
-            (*ntotal)--;
-            printf("\nntotal - %d",*ntotal);
-            buffer  = (void*) realloc (buffer,4*sizeof(int)+(*ntotal)*sizeof(contato));
-        }
-        pessoa++;
-    }
-    //ordena();
-}
-
-void mostraContatos(void) {
-	if ((*ntotal) == 0) printf("\nNao ha nenhum contato ainda\n");
-	else {
-		pessoa = escolha + 1;
-	    for ((*acontador) = 0;(*acontador) < (*ntotal); *acontador = (*acontador) + 1) {
-	    	printf("\nNome - %s\n",pessoa->nome);
-	    	printf("Numero - %d\n\n",pessoa->numero);
-	    	pessoa++;
-	    }
-	}
 }
